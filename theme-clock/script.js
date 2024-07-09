d45ce5eabbe525d9a0290b2abbdf6cd1ced54900
Miho Funayama <funamiho1011@gmail.com>
2024-07-09 10:09:44 +0900

    Added dark mode/light mode toggle function

--- theme-clock/script.js
+++ theme-clock/script.js
@@ -1,6 +1,58 @@
-const hourEl = document.querySelector('.hour')
-const minuteEl = document.querySelector('.minute')
-const secondEl = document.querySelector('.second')
-const timeEl = document.querySelector('.time')
-const dataEl = document.querySelector('.date')
-const toggl = document.querySelector('.toggle')
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dataEl = document.querySelector(".date");
+const toggle = document.querySelector(".toggle");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+// switching between dark/light mode
+toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html')
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark')
+    e.target.innerHTML = 'Dark mode'
+  } else {
+    html.classList.add('dark')
+    e.target.innerHTML = 'Light mode'
+  }
+})
+
+function setTime() {
+  const time = new Date();
+  console.log(time);
+  const month = time.getMonth()
+  const day = time.getDay()
+  const hours = time.getHours()
+  // convert time for am and pm
+  const hoursForClock = hours % 12
+  const minutes = time.getMinutes()
+  const seconds = time.getSeconds()
+
+
+}
+
+setTime()
