f18304b63475ed7d9c3ba4c7e54ff40ac64c5f43
Miho Funayama <funamiho1011@gmail.com>
2024-07-09 09:28:52 +0900

    implemented basic design layout for the clock

---
+++ theme-clock/script.js
@@ -0,0 +1,6 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dataEl = document.querySelector('.date')
+const toggl = document.querySelector('.toggle')
