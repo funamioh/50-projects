55afb5c18c6237e9c5cf58c7d767000ae9cf11aa
Miho Funayama <funamiho1011@gmail.com>
2024-07-09 16:23:13 +0900

    added functionality to fetch time and display

--- theme-clock/script.js
+++ theme-clock/script.js
@@ -2,7 +2,7 @@ const hourEl = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const secondEl = document.querySelector(".second");
 const timeEl = document.querySelector(".time");
-const dataEl = document.querySelector(".date");
+const dateEl = document.querySelector(".date");
 const toggle = document.querySelector(".toggle");
 
 const days = [
@@ -46,13 +46,29 @@ function setTime() {
   console.log(time);
   const month = time.getMonth()
   const day = time.getDay()
+  const date = time.getDate()
   const hours = time.getHours()
   // convert time for am and pm
   const hoursForClock = hours % 12
   const minutes = time.getMinutes()
   const seconds = time.getSeconds()
+  const ampm = hours >= 12 ? 'PM': 'AM';
 
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+  // for minutes and seconds, in_max is 59
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
 
+  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
+// num -> num to convert, in_min -> min number of hour, max -> max number of hour, out_min -> min degree, out_max -> max degree
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
 setTime()
+
+setInterval(setTime, 1000);
